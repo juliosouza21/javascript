8779282c552b40f79c59c374116ea69b1e52f731
julio souza <julio55rap@gmail.com>
2024-06-27 10:49:14 -0300

    Initial commit

---
+++ scripst.js
@@ -0,0 +1,69 @@
+function calcPct(n1, n2){
+return (n2/n1) * 100;
+
+}
+
+
+
+
+
+
+let x =40;
+let y = 10;
+
+let pct= calcPct (x,y);
+console.log (`${pct}% de ${x} é ${y}`);
+
+
+
+
+
+
+
+
+
+
+
+/*function maiorDeIdade (idade){
+    if(idade >= 18) {
+        return true;
+    }else{
+        return false;
+    }
+}
+let idade = 10;
+let verificacao = maiorDeIdade(idade);
+
+if (verificacao){
+    console.log('É maior de idade');
+} else {
+    console.log('Não é maior de idade ');
+}
+*/
+
+
+/*function somar (n1, n2){
+    let resultado = n1 + n2;
+    console.log ('Resultado: ' + resultado);
+
+}
+somar (10,15);*/
+
+
+
+
+
+
+
+
+
+
+
+/*function gravidade () {
+    console.log ('A gravidade do planete é :');
+    console.log (9.8);
+}
+
+console.log (' ola tudo bem');
+
+gravidade();*/
